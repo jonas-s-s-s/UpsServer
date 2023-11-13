76ba04f4dbdc1542a784faa0b6beac96e5c13feb
jonasx <jonasx.dufek@gmail.com>
2023-11-13 15:56:14 +0100

    EPOLL

--- src/linux_commons_lib/epoll_lib/Epoll.cpp
+++ src/linux_commons_lib/epoll_lib/Epoll.cpp
@@ -7,44 +7,43 @@
 #include <fcntl.h>
 #include "Epoll.h"
 
-Epoll::Epoll(bool isEdgeTriggered) : epollFd(epoll_create1(0)), isEdgeTriggered(isEdgeTriggered) {
-    if (epollFd == -1) {
+Epoll::Epoll(bool isEdgeTriggered) : _epollFd(epoll_create1(0)), _isEdgeTriggered(isEdgeTriggered) {
+    if (_epollFd == -1) {
         throw std::runtime_error("Epoll::Epoll: ERROR - Failed to create epoll file descriptor.");
     }
 
-    eventsVector.reserve(maxEventsNum * sizeof(epoll_event));
+    _eventsVector.reserve(_maxEventsNum * sizeof(epoll_event));
 }
 
-void Epoll::addDescriptor(int fd) {
-    monitoredFds.try_emplace(fd, fd);
+//######################################################################################################################
 
-    if (isEdgeTriggered) {
+void Epoll::addDescriptor(int fd) {
+    _monitoredFds.try_emplace(fd, fd);
+
+    if (_isEdgeTriggered) {
         _setNonBlocking(fd);
-        //Set the EPOLLET flag for edge triggered
-        monitoredFds.at(fd).alreadyMonitoredEvents = EPOLLET;
     }
 }
 
 void Epoll::removeDescriptor(int monitoredFd) {
     _epollCtlDelete(monitoredFd);
-    monitoredFds.erase(monitoredFd);
+    _monitoredFds.erase(monitoredFd);
 }
 
 void Epoll::waitForEvents() {
     //Start waiting for descriptor events
-    int numOfEvents = epoll_wait(epollFd, &eventsVector[0], maxEventsNum, -1);
+    int numOfEvents = epoll_wait(_epollFd, &_eventsVector[0], _maxEventsNum, -1);
 
     for (int i = 0; i < numOfEvents; i++) {
-        uint32_t events = eventsVector[i].events;
-        int fd = eventsVector[i].data.fd;
+        uint32_t events = _eventsVector[i].events;
+        int fd = _eventsVector[i].data.fd;
 
-        //Check for all possible events
-        const std::array<uint32_t, 6> samples{EPOLLIN, EPOLLOUT, EPOLLRDHUP, EPOLLPRI, EPOLLERR, EPOLLHUP};
-        for (uint32_t sample: samples) {
+        //Check for all possible event types
+        for (uint32_t evt: allEventTypes) {
             //Check if the handler for this event exists
-            if (monitoredFds.at(fd).handledEvents.count(events & sample)) {
+            if (_monitoredFds.at(fd).hasHandler(events & evt)) {
                 //Call the handler function
-                monitoredFds.at(fd).handledEvents.at(events & sample)(fd);
+                _monitoredFds.at(fd).getHandler(events & evt)(fd);
             }
         }
 
@@ -59,7 +58,7 @@ void Epoll::waitForEvents() {
  * ADD ONLY ONE eventId AT ONCE (TODO?)
  */
 void Epoll::addEventHandler(int monitoredFd, uint32_t eventId, std::function<void(int)> eventHandler) {
-    MonitoredDescriptor &fd = monitoredFds.at(monitoredFd);
+    MonitoredDescriptor &fd = _monitoredFds.at(monitoredFd);
     if (!fd.isInitialized) {
         fd.isInitialized = true;
         //Better solution than alreadyMonitoredEvents?
@@ -74,21 +73,38 @@ void Epoll::addEventHandler(int monitoredFd, uint32_t eventId, std::function<voi
     fd.handledEvents[eventId] = std::move(eventHandler);
 }
 
-void Epoll::removeEventHandler(int monitoredFd, uint32_t eventId) {
+void Epoll::removeEventHandler(int monitoredFd, uint32_t eventType) {
+    //Check for all possible event types
+    for (uint32_t evt: allEventTypes) {
+        //Check if the handler for this event exists
+        if (_monitoredFds.at(monitoredFd).hasHandler(eventType & evt)) {
+            //remove the handler function TODO
+            _monitoredFds.at(monitoredFd).setHandler(eventType & evt, nullptr);
+        }
+    }
+
     //TODO: System call to remove event listening from this fd?
-    monitoredFds.at(monitoredFd).handledEvents.erase(eventId);
+    _monitoredFds.at(monitoredFd).handledEvents.erase(eventType);
 }
 
+//######################################################################################################################
+
 const std::unordered_map<int, MonitoredDescriptor> &Epoll::getMonitoredFds() const {
-    return monitoredFds;
+    return _monitoredFds;
 }
 
 int Epoll::getEpollFd() const {
-    return epollFd;
+    return _epollFd;
 }
 
 int Epoll::getIsEdgeTriggered() const {
-    return isEdgeTriggered;
+    return _isEdgeTriggered;
+}
+
+//######################################################################################################################
+
+void Epoll::_reloadEventHandlers(MonitoredDescriptor &monitoredDescriptor) {
+
 }
 
 /**
@@ -96,14 +112,9 @@ int Epoll::getIsEdgeTriggered() const {
  */
 void Epoll::_epollCtlAdd(int fd, uint32_t events) const {
     struct epoll_event ev{};
-
-    if (isEdgeTriggered)
-        ev.events = events | EPOLLET;
-    else
-        ev.events = events;
-
+    ev.events = events;
     ev.data.fd = fd;
-    if (epoll_ctl(epollFd, EPOLL_CTL_ADD, fd, &ev) == -1) {
+    if (epoll_ctl(_epollFd, EPOLL_CTL_ADD, fd, &ev) == -1) {
         throw std::runtime_error("Epoll::_epollCtlAdd: ERROR - Failed adding event to descriptor.");
     }
 }
@@ -112,16 +123,10 @@ void Epoll::_epollCtlAdd(int fd, uint32_t events) const {
  * REWRITES the events of certain FD. All previously added events will be REMOVED.
  */
 void Epoll::_epollCtlModify(int fd, uint32_t events) const {
-    //TODO: Improve?
     struct epoll_event ev{};
-
-    if (isEdgeTriggered)
-        ev.events = events | EPOLLET;
-    else
-        ev.events = events;
-
+    ev.events = events;
     ev.data.fd = fd;
-    if (epoll_ctl(epollFd, EPOLL_CTL_MOD, fd, &ev) == -1) {
+    if (epoll_ctl(_epollFd, EPOLL_CTL_MOD, fd, &ev) == -1) {
         throw std::runtime_error("Epoll::_epollCtlModify: ERROR - Failed modifying file descriptor events.");
     }
 }
@@ -135,8 +140,74 @@ void Epoll::_setNonBlocking(int fd) {
 void Epoll::_epollCtlDelete(int fd) const {
     struct epoll_event ev{};
     ev.data.fd = fd;
-    epoll_ctl(epollFd, EPOLL_CTL_DEL, fd, &ev);
+    epoll_ctl(_epollFd, EPOLL_CTL_DEL, fd, &ev);
 }
 
+//######################################################################################################################
+
 MonitoredDescriptor::MonitoredDescriptor(int monitoredFd) : monitoredFd(monitoredFd) {
+}
+
+
+bool MonitoredDescriptor::hasHandler(uint32_t eventType) const {
+    switch (eventType) {
+        case EPOLLIN:
+            return IN_handler != nullptr;
+        case EPOLLOUT:
+            return OUT_handler != nullptr;
+        case EPOLLRDHUP:
+            return RDHUP_handler != nullptr;
+        case EPOLLPRI:
+            return PRI_handler != nullptr;
+        case EPOLLERR:
+            return ERR_handler != nullptr;
+        case EPOLLHUP:
+            return HUP_handler != nullptr;
+        default:
+            return false;
+    }
+}
+
+void MonitoredDescriptor::setHandler(uint32_t eventType, std::function<void(int)> handler) {
+    switch (eventType) {
+        case EPOLLIN:
+            IN_handler = std::move(handler);
+            return;
+        case EPOLLOUT:
+            OUT_handler = std::move(handler);
+            return;
+        case EPOLLRDHUP:
+            RDHUP_handler = std::move(handler);
+            return;
+        case EPOLLPRI:
+            PRI_handler = std::move(handler);
+            return;
+        case EPOLLERR:
+            ERR_handler = std::move(handler);
+            return;
+        case EPOLLHUP:
+            HUP_handler = std::move(handler);
+            return;
+        default:
+            return;
+    }
+}
+
+std::function<void(int)> &MonitoredDescriptor::getHandler(uint32_t eventType) {
+    switch (eventType) {
+        case EPOLLIN:
+            return IN_handler;
+        case EPOLLOUT:
+            return OUT_handler;
+        case EPOLLRDHUP:
+            return RDHUP_handler;
+        case EPOLLPRI:
+            return PRI_handler;
+        case EPOLLERR:
+            return ERR_handler;
+        case EPOLLHUP:
+            return HUP_handler;
+        default:
+            throw std::runtime_error("Epoll::MonitoredDescriptor::getHandler: ERROR - passed eventType is invalid.");
+    }
 }
--- src/linux_commons_lib/epoll_lib/include/Epoll.h
+++ src/linux_commons_lib/epoll_lib/include/Epoll.h
@@ -9,15 +9,39 @@
 #include <functional>
 #include <sys/epoll.h>
 
+constexpr static const std::array<uint32_t, 6> allEventTypes{EPOLLIN, EPOLLOUT, EPOLLRDHUP, EPOLLPRI, EPOLLERR,
+                                                             EPOLLHUP};
+
 class MonitoredDescriptor {
 public:
     explicit MonitoredDescriptor(int monitoredFd);
 
     bool isInitialized = false;
     const int monitoredFd;
-    std::unordered_map<uint32_t, std::function<void(int)>> handledEvents{}; //Can be replaced with 6 attributes / array?
 
+    /**
+     * Checks if this eventType has a handler function assigned to it
+     */
+    bool hasHandler(uint32_t eventType) const;
 
+    /**
+     * Sets an event handler of a SINGLE eventType (don't use | bitwise or notation)
+     */
+    void setHandler(uint32_t eventType, std::function<void(int)> handler);
+
+    /**
+     * Gets the events handler associated with this SINGLE eventType
+     */
+    std::function<void(int)>& getHandler(uint32_t eventType);
+
+private:
+    //No need to use unordered_map since there are only 6 possible event types
+    std::function<void(int)> IN_handler = nullptr;
+    std::function<void(int)> OUT_handler = nullptr;
+    std::function<void(int)> RDHUP_handler = nullptr;
+    std::function<void(int)> PRI_handler = nullptr;
+    std::function<void(int)> ERR_handler = nullptr;
+    std::function<void(int)> HUP_handler = nullptr;
 };
 
 class Epoll {
@@ -32,7 +56,7 @@ public:
 
     void addEventHandler(int monitoredFd, uint32_t eventId, std::function<void(int)> eventHandler);
 
-    void removeEventHandler(int monitoredFd, uint32_t eventId);
+    void removeEventHandler(int monitoredFd, uint32_t eventType);
 
     const std::unordered_map<int, MonitoredDescriptor> &getMonitoredFds() const;
 
@@ -41,12 +65,14 @@ public:
     int getIsEdgeTriggered() const;
 
 private:
-    std::unordered_map<int, MonitoredDescriptor> monitoredFds{};
-    const int epollFd;
-    const int isEdgeTriggered;
+    std::unordered_map<int, MonitoredDescriptor> _monitoredFds{};
+    const int _epollFd;
+    const int _isEdgeTriggered;
 
-    const int maxEventsNum = 10;
-    std::vector<epoll_event> eventsVector{};
+    const int _maxEventsNum = 10;
+    std::vector<epoll_event> _eventsVector{};
+
+    void _reloadEventHandlers(MonitoredDescriptor& monitoredDescriptor);
 
     void _epollCtlAdd(int fd, uint32_t events) const;
 
